a05f8f4452999a35b3557288457f6f1208104053
DuyQK <duyqk@runsystem.net>
2017-10-24 10:35:18 +0700

    add lib

--- App.js
+++ App.js
@@ -9,23 +9,51 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
+
+var MemoryStore = require('./lib/memoryStore');
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
+  'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component<{}> {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      test: 1
+    }
+  }
   render() {
+    console.log('====', MemoryStore);
+    
+    MemoryStore.getItem('key', (err, value) => {
+      console.log(err, value)
+    })
+
+    if (this.state.test == 4){
+      MemoryStore.setItem('key', 'Quang Khuong Duy', (er) => {
+        console.log(er);
+      })
+    }
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <TouchableOpacity onPress={()=>{
+          this.setState({
+            test: this.state.test +1
+          })
+        }}>
+          <Text style={styles.welcome}>
+            Welcome {this.state.test} !
         </Text>
+        </TouchableOpacity>
         <Text style={styles.instructions}>
           To get started, edit App.js
         </Text>
